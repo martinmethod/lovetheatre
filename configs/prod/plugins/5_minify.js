73aa592a7ee6b5e7ba7d106c7de1a5908bb8b577
Martin Metodiev <metodiev.martin@gmail.com>
2019-01-29 11:13:27 +0200

    Fix configuration of MinifyPlugin

--- configs/prod/plugins/5_minify.js
+++ configs/prod/plugins/5_minify.js
@@ -9,7 +9,7 @@ const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 //--------------------------| Configuration
 
-const plugin = new MinifyPlugin({
+const plugin = new MinifyPlugin({}, {
   comments: false
 });
 
